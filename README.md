0a57aa171368c00c97f62d5f36748099adefdab3
tengfei <tengfei.qi@gmail.com>
2024-11-20 12:33:04 +0800

    initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# file-path-header
+
+A Node.js tool to automatically add file paths as comments to source files.
+
+## Installation
+
+```bash
+npm install -g file-path-header
---
+++ bin/cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const path = require('path');
+const FilePathHeader = require('../lib/index');
+
+program
+  .version('1.0.0')
+  .description('Add file paths as comments to source files')
+  .argument('[directory]', 'Target directory', './src')
+  .option('-i, --ignore <patterns...>', 'Files to ignore')
+  .option('-c, --config <path>', 'Path to config file')
+  .action(async (directory, options) => {
+    try {
+      const targetDir = path.resolve(process.cwd(), directory);
+
+      let config = {};
+      if (options.config) {
+        config = require(path.resolve(process.cwd(), options.config));
+      }
+
+      const filePathHeader = new FilePathHeader({
+        ignoreFiles: options.ignore || config.ignoreFiles || [],
+        customComments: config.customComments || {}
+      });
+
+      await filePathHeader.addFilePathComments(targetDir);
+      console.log('Successfully processed all files!');
+    } catch (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
+    }
+  });
+
+program.parse(process.argv);
